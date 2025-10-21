8212465093ec56b63357c7a97866b995d349c226
Akram <akram.maouche07@gmail.com>
2025-10-21 13:29:01 +0100

    ETL Piplines

--- push_data.py
+++ push_data.py
@@ -29,8 +29,8 @@ class NetworkDataExtract():
 
     def csv_to_json_convertor(self, file_path): 
         try : 
-            data = pd.read_csv(file_path)
-            data.reindex(drop = True, inplase = True) 
+            data = pd.read_csv(file_path) 
+            data.reset_index(drop=True, inplace=True)
             records = list(json.loads(data.T.to_json()).values())
             return records
 
@@ -47,11 +47,22 @@ class NetworkDataExtract():
             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
             self.database  = self.mongo_client[self.database]
 
-            self.collection = self.database[]
-
-
-
-
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            print(len(self.records))
+            return (len(self.records))
         except Exception as e : 
             raise NetworkSecurityException(e,sys)
+
+
+if __name__ == '__main__': 
+    FILE_PATH  = "Network_Data/phisingData.csv"
+    DATABASE = "AGhilazAiDB"
+    Collection = 'NetworkData'
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records) 
+    no_of_records = networkobj.insert_data_mongodb(records=records,database=DATABASE,
+                                                   collection=Collection)
+
 
